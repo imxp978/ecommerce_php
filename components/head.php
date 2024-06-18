ba6df2263f53abaf99af9c1c773e1091d998ded8
chun <quelquefoi@gmail.com>
2024-06-18 19:00:49 +0800

    sketch added

--- components/head.php
+++ components/head.php
@@ -1,6 +1,6 @@
 <?php require_once('connections/conn_db.php'); ?>
 <?php if (session_status() === PHP_SESSION_NONE) { session_start(); } ?>
-<?php require_once("./components/php_lib.php"); ?>
+<?php require_once("../components/php_lib.php"); ?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -13,8 +13,6 @@
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
-
-
   <link rel="stylesheet" href="./main.css" />
 </head>
 <body>
